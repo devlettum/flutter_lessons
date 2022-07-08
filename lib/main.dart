265f92365679d15cdb4cd5cc8164dafaefbaffb0
Batuhan ARIK <batuhanarik123@gmail.com>
2022-07-08 13:06:55 +0300

    Homework 1 was done.

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,69 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  Container containerOlustur(String harf,Color renk,{double margin=0}){
+    return Container(
+       
+          width: 75,
+          height: 75,
+          alignment: Alignment.center,
+          margin:EdgeInsets.only(top:margin),
+          color: renk,
+          child: Text(
+            harf,
+            style: TextStyle(fontSize: 48),
+          ),
+    );
+  }  
+
+
+
+  Row dartRowunuOlustur() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        containerOlustur("D",Colors.orange.shade200),
+        containerOlustur("A",Colors.orange.shade300),
+        containerOlustur("R",Colors.orange.shade400),
+        containerOlustur("T",Colors.orange.shade500),
+     ],
+    );
+  }
+
+
+
+
+
+  Column dersleriColumnOlustur() {
+    return Column(
+      mainAxisAlignment:MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          child:containerOlustur('E',Colors.orange.shade300,margin:15),
+        ),
+        Expanded(
+          child:containerOlustur('R',Colors.orange.shade400,margin:15),
+        ),
+        Expanded(
+          child:containerOlustur('S',Colors.orange.shade500,margin:15),
+        ),
+        Expanded(
+          child:containerOlustur('L',Colors.orange.shade600,margin:15),
+        ),
+        Expanded(
+          child:containerOlustur('E',Colors.orange.shade700,margin:15),
+        ),
+        Expanded(
+          child:containerOlustur('R',Colors.orange.shade800,margin:15),
+        ),
+        Expanded(
+          child:containerOlustur('İ',Colors.orange.shade900,margin:15),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,28 +82,12 @@ class MyApp extends StatelessWidget {
         // backgroundColor: Colors.yellow,
         appBar: AppBar(title: Text('Batuhan'), backgroundColor: Colors.amber),
         body: Container(
-          color: Colors.red.shade200,
-          // height:400,
+          color: Colors.red.shade100,
           child: Column(
-            /*ROW ile kullandık. */
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('B'),
-                  Text('A'),
-                  Text('T'),
-                  Text('U'),
-                ],
-              ),
-              Icon(Icons.add_circle, size: 64, color: Colors.green),
-              Icon(Icons.add_circle, size: 64, color: Colors.red),
-              Icon(Icons.add_circle, size: 64, color: Colors.blue),
-              Icon(Icons.add_circle, size: 64, color: Colors.orange),
+            children: [
+              dartRowunuOlustur(),
+              Expanded(child: dersleriColumnOlustur()),
             ],
           ),
         ),
@@ -52,50 +99,6 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.red,
         ),
       ),
-    );
-  }
-
-  Center containerLessons() {
-    return Center(
-      // alignment: Alignment.center,
-      // alignment:Alignment(1,-1),
-      // alignment:Alignment.topLeft,
-      // color:Colors.purple,
-      // width:200,
-      // height:200,
-      // child:Text('batu',textAlign: TextAlign.center),
-      // width:200,
-      // height:200,
-      // margin:EdgeInsets.all(16),
-      // margin:EdgeInsets.only(left:8,top:20),
-      // margin:EdgeInsets.symmetric(vertical:50, horizontal:50),
-      // margin:EdgeInsets.fromLTRB(10,20,50,30),
-      // padding:EdgeInsets.all(30),
-      // constraints: BoxConstraints(minWidth:100,minHeight:100,maxWidth:200,maxHeight:200),
-
-      // width:100,
-      // height:100,
-      // color:Colors.purpleAccent,
-      // // alignment:Alignment.center,
-      // child:Container(
-      //   alignment:Alignment.center,
-      //   color:Colors.blue,
-      //   margin:EdgeInsets.all(20),
-      //   width:50,
-      //   height:50,
-      //   child:Text('BATU'),
-      // ),
-
-      //padding:EdgeInsets.all(20),
-      child: FlutterLogo(
-        size: 128,
-        // textColor:Colors.purple,
-        // style:FlutterLogoStyle.stacked,
-      ),
-      //decoration: BoxDecoration(
-      //color:Colors.orange,
-      //shape:BoxShape.circle
-      //),
     );
   }
 }
